4ba697967e2838cc26a2cf354ca05853b3883054
Thomas Kok <thomas.kok@amsterdam.nl>
2022-05-02 13:21:07 +0200

    Adjust recources

--- dags/garageparkeren/test.py
+++ dags/garageparkeren/test.py
@@ -57,10 +57,10 @@ with DAG(
         image=IMAGE,
         job_script_path="/app/src/spark_test.py",
         spark_driver_cores=1,
-        spark_driver_memory_gb=8,
-        spark_executor_cores=2,
-        spark_executor_memory_gb=8,
-        spark_executor_instances=3,
+        spark_driver_memory_gb=1,
+        spark_executor_cores=1,
+        spark_executor_memory_gb=1,
+        spark_executor_instances=1,
         # Source sytem is nodig voor to datamart jobs
         source_system="ski3",
     )

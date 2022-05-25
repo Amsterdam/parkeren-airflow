35e10e24df0b8874f5d47ed124a5f4f181ba4d25
Thomas Kok <thomas.kok@amsterdam.nl>
2022-05-25 11:54:33 +0200

    Add migrate

--- dags/garageparkeren/common.py
+++ dags/garageparkeren/common.py
@@ -22,8 +22,11 @@ def generate_job(
     spark_executor_memory_gb: int = 1,
     spark_executor_instances: int = 1,
     #     TODO change naming
-    arguments: List[str] = field(default_factory=list),
+    arguments: List[str] = None,
 ) -> client.V1Job:
+    if arguments is None:
+        arguments = ()
+
     kubernetes_memory_mb = int(
         ((1 + KUBERNETES_MEMORY_OVERHEAD_FACTOR) * 1000) * spark_driver_memory_gb
     )
@@ -147,7 +150,7 @@ class SparkJob:
     python_path: str
     spark_driver_cores: int = 1
     spark_executor_cores: int = 1
-    arguments: List[str] = field(default_factory=list)
+    arguments: List[str] = None
 
 
 def add_job_to_node(

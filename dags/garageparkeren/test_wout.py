0827ea58280c251aca76a6dc6dafb59bb79dd4d0
Wout Snijders <w.snijders@amsterdam.nl>
2022-05-31 11:29:47 +0200

    adopt for wout usage

--- dags/garageparkeren/test_wout.py
+++ dags/garageparkeren/test_wout.py
@@ -61,7 +61,7 @@ with DAG(
             ),
             namespace=NAMESPACE,
             image="parkerenweuacrow77kin67.azurecr.io/parkeren-spark:thomas",
-            job_script_path="/app/src/jobs/master_data/run_all_master.py",
+            job_script_path="/app/src/spark_thomas.py",
             spark_driver_cores=1,
             spark_driver_memory_gb=1,
             spark_executor_cores=2,
